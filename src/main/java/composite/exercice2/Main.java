5f7348cbce4378b5999752e48af5d454ec4b8ac4
Julien <kalaycijulien@gmail.com>
2022-06-09 23:59:10 +0200

    Composite - 2 Fini, prix total affiché, calcul fait avec boucle entrySet des components

--- src/main/java/composite/exercice2/Main.java
+++ src/main/java/composite/exercice2/Main.java
@@ -32,29 +32,7 @@ public class Main {
         pizza.addSousRecette(1, pate);
         pizza.add(2, mozzarella);
 
-        Double prixTotal = 0d;
-        //on recupère les ingrédients de Pizza
-        for (Map.Entry<Ingredient, Double> mapIngredientsPizza : pizza.getIngredients().entrySet()) {
-            Ingredient ingredientDePizza = mapIngredientsPizza.getKey();
-            Double quantiteIngredientPizza = mapIngredientsPizza.getValue();
-            prixTotal +=  ingredientDePizza.getPrix() * quantiteIngredientPizza;
-
-            //On recupère toutes les sous recettes de pizza
-            for(Recette sousRecette : pizza.getSousRecettes().keySet()){
-
-                //On veut recup quantité et Ingredient (pour recup prix) pour faire quantite * prix
-                for (Map.Entry<Ingredient, Double> mapIngredientsDeSousRecette : sousRecette.getIngredients().entrySet()) {
-                    Ingredient ingredientDeSousRecette = mapIngredientsDeSousRecette.getKey();
-                    Double quantiteIngredient = mapIngredientsDeSousRecette.getValue();
-                    prixTotal +=  ingredientDeSousRecette.getPrix() * quantiteIngredient;
-                }
-            }
-        }
-        //Pour rendre le resultat plus lisible avec 3 décimal
-        DecimalFormat df = new DecimalFormat("#.###");
-        df.setRoundingMode(RoundingMode.FLOOR);
-        String result =  df.format(prixTotal);
-        System.out.println(result);
+        System.out.println(pizza.getPrix());
 
         // 1. Calculer le prix d'une recette de manière naïve = sans le pattern Composite
 
--- src/main/java/composite/exercice2/Recette.java
+++ src/main/java/composite/exercice2/Recette.java
@@ -1,42 +1,29 @@
 package composite.exercice2;
 
-import composite.exercice.DossierComponent;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class Recette implements CookingComponent{
-    private Map<Ingredient, Double> ingredients = new HashMap<>();
-    private Map<Recette, Double> sousRecettes = new HashMap<>();
 
-    private List<CookingComponent> components = new ArrayList<>();
+    private Map<CookingComponent, Double> components = new HashMap<>();
 
 
     public void add(double quantite, Ingredient ingredient) {
-        ingredients.put(ingredient, quantite);
+        components.put(ingredient, quantite);
     }
 
     public void addSousRecette(double quantite, Recette recette) {
-        sousRecettes.put(recette, quantite);
+        components.put(recette, quantite);
     }
-
-    public Map<Ingredient, Double> getIngredients() {
-        return ingredients;
-    }
-
-    public Map<Recette, Double> getSousRecettes() {
-        return sousRecettes;
-    }
-
     @Override
     public double getPrix() {
-        double prixTotale = 0;
-        for (CookingComponent component : components) {
-            double prix = component.getPrix();
-            prixTotale += prix;
+        double prixTotal = 0;
+        for (Map.Entry<CookingComponent, Double> mapIngredientsPizza : components.entrySet()) {
+            CookingComponent ingredientDePizza = mapIngredientsPizza.getKey();
+            Double quantiteIngredientPizza = mapIngredientsPizza.getValue();
+            prixTotal +=  ingredientDePizza.getPrix() * quantiteIngredientPizza;
         }
-        return prixTotale;
+        return prixTotal;
     }
 }
